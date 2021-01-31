2a503a8684e07f72e2f38f438e750f39df5039c5
drew-bozarth <dbozarth@chapman.edu>
2021-01-31 07:38:11 +0000

    tried to add comments

--- intarray.cpp
+++ intarray.cpp
@@ -8,24 +8,24 @@ Assignment 2 - intarray */
 #include <vector>
 using namespace std;
 
-int main() {
+int main() { //main part of program
 
-  int NUM_VALS = 10;
-  vector<int> numArray(NUM_VALS);
-  int current_Num;
-  int i = 0;
+  int NUM_VALS = 10; //sets the number of values wanted in array
+  vector<int> numArray(NUM_VALS); //creates a vector with 10 slots
+  int current_Num; //sets int variable
+  int i = 0; //sets up i which is our iteration variable
 
-  while (i != 10) {
-    cout << "Please enter a positive integer: ";
-    cin >> current_Num;
-    if (current_Num >= 0) {
-      numArray.at(i) = current_Num;
-      ++i;
+  while (i != 10) { //loop runs while i isnt 10
+    cout << "Please enter a positive integer: "; //prompts for a positive num
+    cin >> current_Num; //gets user input
+    if (current_Num >= 0) { //if branch executes if the number entered is positive
+      numArray.at(i) = current_Num; //assigns the current iteration in the vector with the current enter num
+      ++i; //increments i
     }
   }
 
-  for (i = 0; i < numArray.size(); i++) {
-    cout << "Number " << i + 1 << ": " << numArray.at(i) << endl;
+  for (i = 0; i < numArray.size(); i++) { //loop runs all the way through the vector
+    cout << "Number " << i + 1 << ": " << numArray.at(i) << endl; //output printing each place of the user's numbers with their numbers
   }
 
   return 0;
